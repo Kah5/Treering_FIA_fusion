f186e64c0bdc08a5fb34ce10ea91240db5fc1672
Kah5 <kellyannheilman@gmail.com>
2024-12-31 12:15:57 -0700

    updating regional map code to output map objects for use in figure 2

--- R/Regional_map.R
+++ R/Regional_map.R
@@ -10,7 +10,7 @@ library(ggplot2)
 library(tidyr)
 library(tidyverse)
 library(mapdata)
-library(maptools)
+#library(maptools)
 
 # make sure that Rdriver_local_regional.R has been run
 
@@ -29,7 +29,7 @@ states <- subset(all_states, region %in% c( "arizona", "utah", "new mexico", "co
 canada <- map_data("worldHires", "Canada")
 mexico <- map_data("worldHires", "Mexico")
 
-dir.path = "/Users/kellyheilman/USTreeAtlas/shp/" # where the distribution shape files are
+dir.path = "/Users/kellyheilman/Documents/Treering_FIA_fusion_PIPO/data/" # where the distribution shape files are
 
 spp <- "pinupond"
 spp.distribution <- st_read(paste0(dir.path, spp, "/"))
@@ -37,11 +37,11 @@ spp.distribution <- st_read(paste0(dir.path, spp, "/"))
 
 if(file.exists(paste0(dir.path, spp, "/"))){
   spp.distribution <- st_read(paste0(dir.path, spp, "/"))
-  
-  plt.spp <- plt.shp %>% filter(shp.code %in% spp)
+  #plt.shp <- spp.distribution
+  #plt.spp <- plt.shp %>% filter(shp.code %in% spp)
   
   # plot distribution 
-  spp.distribution %>% 
+ US.spp.map <-  spp.distribution %>% 
     ggplot() +
     geom_polygon(data = states, 
                  aes(x=long, y=lat, group = group), 
@@ -56,10 +56,10 @@ if(file.exists(paste0(dir.path, spp, "/"))){
     scale_fill_manual(values = c("1" = "forestgreen", "0" = "white"))+
     geom_point(data = cov.data.regional, aes(x = LON, y = LAT), size = 0.5)+theme_bw()+
     coord_sf(xlim = c(-118, -103), ylim = c(32, 49))+theme(axis.title = element_blank(), legend.position = "none")
+US.spp.map
+  ggsave( height = 6, width = 8, units = "in", here("outputs/cored_pipo_distn_", paste0(spp, "_distribution_map_zoom.png")))
   
-  ggsave(height = 6, width = 8, units = "in", here("outputs/cored_pipo_distn_", paste0(spp, "_distribution_map_zoom.png")))
-  
-  spp.distribution %>% 
+  full.spp.map <- spp.distribution %>% 
     ggplot() +
     geom_polygon(data = states, 
                  aes(x=long, y=lat, group = group), 
@@ -74,9 +74,10 @@ if(file.exists(paste0(dir.path, spp, "/"))){
     scale_fill_manual(values = c("1" = "forestgreen", "0" = "white"))+
     geom_point(data = cov.data.regional, aes(x = LON, y = LAT), size = 0.5)+theme_bw()+
     coord_sf(xlim = c(-125, -100), ylim = c(20, 52))+theme(axis.title = element_blank(), legend.position = "none")
-  
+  full.spp.map
   ggsave(height = 6, width = 8, units = "in", here("outputs/cored_pipo_distn_", paste0(spp, "_distribution_map_full.png")))
   
   
   }  
+
 
